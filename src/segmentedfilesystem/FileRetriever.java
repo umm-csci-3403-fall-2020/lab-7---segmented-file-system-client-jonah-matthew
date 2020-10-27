b71d196f3458eee369e68c33d3894d453c2edb05
Jonah-Northwood <north305@morris.umn.edu>
2020-10-27 10:46:25 -0500

    Start work in FileRetriever.java

--- src/segmentedfilesystem/FileRetriever.java
+++ src/segmentedfilesystem/FileRetriever.java
@@ -1,13 +1,18 @@
 package segmentedfilesystem;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class FileRetriever {
 
-	public FileRetriever(String server, int port) {
+        public FileRetriever(String server, int port) {
         // Save the server and port for use in `downloadFiles()`
         //...
+        // (I'm not sure what this means)
+                downloadFiles(server, port);
 	}
 
-	public void downloadFiles() {
+	public void downloadFiles(String server, int port) {
         // Do all the heavy lifting here.
         // This should
         //   * Connect to the server
@@ -20,6 +25,21 @@ public class FileRetriever {
         // PacketManager.allPacketsReceived() that you could
         // call for that, but there are a bunch of possible
         // ways.
+
+                DatagramSocket socket = new DatagramSocket();
+                byte[] sendBuf = new byte[1028];
+                InetAddress address = InetAddress.getByName(server);
+        
+                byte[] buf = new byte[1028];
+                DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
+                socket.send(packet);
+        
+                while(/*loop until you have all the packets*/){
+                        packet = new DatagramPacket(buf, buf.length);
+                        socket.receive(packet);
+                        // Give packet to PacketManager.java
+                }
+                socket.close();
 	}
 
 }
